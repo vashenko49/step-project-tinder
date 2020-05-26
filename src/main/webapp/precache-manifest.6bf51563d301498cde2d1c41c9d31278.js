58ac46ff5fe3149ff00006ebcd0b9b24146eab64
Vashchenko Sergii <vashenko49@gmail.com>
2020-05-27 02:50:10 +0300

    prerare to deploy

---
+++ src/main/webapp/precache-manifest.6bf51563d301498cde2d1c41c9d31278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eaee4e09f67bfca0905924618f83b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b00342f2a0dbe7a9207",
+    "url": "/static/css/2.b4fceb64.chunk.css"
+  },
+  {
+    "revision": "4b4932077af2d45df49f",
+    "url": "/static/css/main.5a69e803.chunk.css"
+  },
+  {
+    "revision": "1b00342f2a0dbe7a9207",
+    "url": "/static/js/2.e3fcd22f.chunk.js"
+  },
+  {
+    "revision": "ea1a3a54e30f400c776f361fbbdf3976",
+    "url": "/static/js/2.e3fcd22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4932077af2d45df49f",
+    "url": "/static/js/main.eea81c56.chunk.js"
+  },
+  {
+    "revision": "7edf6d9eb97584f691c9",
+    "url": "/static/js/runtime-main.c18f261e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
